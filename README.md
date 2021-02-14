23615f2c8108f1d8d65aae4c467e0a53365535fe
WoodProgrammer <emirozbir@kloia.com>
2021-02-14 23:37:14 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # thebehzo
 AWS Ssm Connector to able access EC2 instance without recheck dashboard or connect to vpn
+
+# Usage
+Example Usage: 
+
+```
+    $ thebehzo eu-west-
+    > kubernetes-worker-1 , i-INSTANCE_ID
+    > kubernetes-master , i-INSTANCE_ID
+
+```
+
+You can select easily with interactive menu
+<img src="./img/demo.png"></img>
+
+# Setup
+You can install python3 dependencies like that :
+```
+    $ pip3 install -r requirements.txt
+```
+After than you should specify default AWS_PROFILE, or credential values into your terminal session (I prefer profile usage):
+
+```
+    $ export AWS_PROFILE=<MY_ACCOUNT_ID> AWS_DEFAULT_REGION=<REGION> AWS_SDK_LOAD_CONFIG=1
+```
+
+Notice : If you want to run this via your shell, you should move somewhere in your $PATH directory:
---
+++ thebehzo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from simple_term_menu import TerminalMenu
+import boto3
+import sys
+import os
+import subprocess
+
+def getRunningInstance(ec2Client):
+
+	gatheredInstances = []
+	instances = ec2Client.instances.filter(Filters=[{
+		'Name': 'instance-state-name',
+		'Values': ['running']}])
+
+	for instance in instances:
+		for tag in instance.tags:
+			if 'Name'in tag['Key']:
+				name = tag['Value']
+				tmp_instace = ""
+				tmp_instace = "{} , {}".format(name, instance.id)
+				gatheredInstances.append(tmp_instace)
+
+	return gatheredInstances
+
+
+def startSession(instanceKey):
+	instanceId = instanceKey.split(" , ")[1]
+
+	if instanceId.startswith("i-"):
+		cmd = ['aws', 'ssm', 'start-session', '--target', instanceId]
+		env = os.environ.copy()
+		p = subprocess.Popen(cmd, env=env)
+
+		while True:
+			try:
+				p.wait()
+				break
+			except KeyboardInterrupt as e:
+				pass
+	else:
+		print("Invalid instance id {}".format(instanceId))
+
+def connector(ec2Client):
+	instances = getRunningInstance(ec2Client)
+
+	if instances == None or instances == '':
+		print("Error: There is no running in specific conditions")
+		sys.exit(1)
+
+	else:
+		terminal_menu = TerminalMenu(instances)
+		menu_entry_index = terminal_menu.show()
+		startSession(instances[menu_entry_index])
+
+
+if __name__ == "__main__":
+	try:
+		region = sys.argv[1]
+
+	except Exception as exp:
+		print("Index error on usage behzo <REGION-NAME>, eu-west-1 using as default")
+		region = "eu-west-1"
+
+	ec2Client = boto3.resource("ec2", region_name=region)
+
+	connector(ec2Client)
